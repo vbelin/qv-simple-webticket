c89d59214a5b201e0664a91d8b108b747b96d340
Rikard Braathen <rikard.braathen@gmail.com>
2013-08-01 08:54:22 +0200

    Initializing...

---
+++ SimpleWebTicket/CommHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace SimpleWebTicket
+{
+    public partial class Default
+    {
+        public string Execute(string address, string method = "GET", string data = "")
+        {
+            try
+            {
+                if (method == "POST")
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(address);
+                    if (!Anonymous)
+                    {
+                        if (UserName == "" && Password == "")
+                            request.UseDefaultCredentials = true;
+                        else
+                            request.Credentials = new NetworkCredential(UserName, Password);
+
+                        request.PreAuthenticate = true;
+                    }
+                    request.Method = method;
+                    request.Timeout = 5000;
+                    request.ContentType = "application/x-www-form-urlencoded";
+                    request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+                    var buffer = Encoding.UTF8.GetBytes(data);
+                    request.ContentLength = buffer.Length;
+                    var dataStream = request.GetRequestStream();
+                    dataStream.Write(buffer, 0, buffer.Length);
+                    dataStream.Close();
+
+                    var response = (HttpWebResponse)request.GetResponse();
+                    var responseStream = response.GetResponseStream();
+                    var reader = new StreamReader(responseStream, Encoding.UTF8);
+                    var result = reader.ReadToEnd();
+
+                    reader.Close();
+                    dataStream.Close();
+                    response.Close();
+
+                    return result;
+                }
+                else
+                {
+                    var req = WebRequest.Create(address.ToString());
+                    var resp = req.GetResponse();
+                    var sr = new StreamReader(resp.GetResponseStream());
+                    return sr.ReadToEnd().Trim();
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+
+}
---
+++ SimpleWebTicket/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SimpleWebTicket
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        // If TrustedIPs are used in c:\ProgramData\QlikTech\WebServer\config.xml this should be set to true, otherwise false
+        private const bool Anonymous = false;
+        // If Windows Authentication is used for GetWebTicket.aspx then credentials may be specified below. If no credentials is provided, UseDefaultCredentials will be used
+        private const string UserName = "";
+        private const string Password = "";
+        
+        // URL that the user is redirected to after a successful login. AccessPoint is usually where you want to go
+        private const string TryUrl = "/QlikView/";
+        // URL redirected to after a failed login attempt
+        private const string BackUrl = "";
+        // Server where the QlikView AccessPoint resides (ends with slash)
+        private const string AccessPointServer = "http://localhost/";
+        
+        // Variable declarations (DON'T CHANGE HERE)
+        private string _userId = "";
+        private string _userFriendlyName = "";
+        private string _userGroups = "";
+        private string _webTicket = ""; 
+ 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // [REQUIRED] Name of the user to get a ticket for
+            _userId = "rfn@qlikview.com";
+            // [OPTIONAL] A friendly name for the user. For example if the username is a social security number of phonenumber the friendly name could be his/hers real name
+            _userFriendlyName = "Rikard Braathen";
+            // [OPTIONAL] Semicolon separated string with groups/roles the user belongs to for use with Section Access or authorization
+            _userGroups = "PreSales;Europe;Stockholm Office";
+
+            GetWebTicket();
+
+            if (!String.IsNullOrEmpty(_webTicket))
+                RedirectToQlikView();
+            else
+                Response.Write("An error occured!");
+        }
+    }
+}
